f0eaab48f87ae261c9d2eefee2625f629f023fe9
gsy <1007691667@qq.com>
2017-01-23 15:06:24 +0800

    first commit

---
+++ app/src/main/java/com/example/hp/map/MainActivity.java
@@ -0,0 +1,83 @@
+package com.example.hp.map;
+
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.baidu.location.BDLocation;
+import com.baidu.location.BDLocationListener;
+import com.baidu.location.LocationClient;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.jar.Manifest;
+
+public class MainActivity extends AppCompatActivity {
+    public LocationClient mLocationClient;
+    private TextView positionText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mLocationClient = new LocationClient(getApplicationContext());
+        mLocationClient.registerLocationListener(new MyLocationListener());
+        setContentView(R.layout.activity_main);
+        positionText = (TextView) findViewById(R.id.position_text_view);
+        List<String> permissionList=new ArrayList<>();
+        if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_FINE_LOCATION))
+            !=PackageManager.PERMISSION_GRANTED){
+            permissionList.add(Manifest.permisson.ACCESS_FINE_lOCATION);
+        }
+    }
+
+
+    private void requestLocation(){
+        mLocationClient.start();
+
+    }
+
+    @Override
+    public void onRequestPermissonResult(int requestCode, String[] permissons,int[] grantResults){
+        switch (requestCode){
+            case 1:
+                if (grantResults.length>0){
+                    for (int result :grantResults){
+                        if (result!=PackageManager.PERMISSION_GRANTED){
+                            Toast.makeText(this,"必须同意所有的权限才能使用本程序",Toast.LENGTH_SHORT).show();
+                            finish();
+                            return;
+                        }
+                    }
+                    requestLocation();
+                }else {
+                    Toast.makeText(this,"发生未知错误",Toast.LENGTH_SHORT).show();
+                    finish();
+                }
+                break;
+            default:
+        }
+    }
+
+    public class MyLocationListener implements BDLocationListener{
+
+        @Override
+        public void onReceiveLocation(BDLocation location){
+            StringBuilder currentPosition = new StringBuilder();
+            currentPosition.append("维度：").append(location.getLatitude()).append("\n");
+            currentPosition.append("经度:").append(location.getLongitude()).append("\n");
+            currentPosition.append("定位方式");
+            if (location.getLocType()==BDLocation.TypeGpsLocation){
+                currentPosition.append("GPS");
+            }else if (location.getLocType()==BDLocation.TypeNetWorkLocation){
+                currentPosition.append("网络");
+
+            }
+            positionText.setText(currentPosition);
+        }
+    }
+}
